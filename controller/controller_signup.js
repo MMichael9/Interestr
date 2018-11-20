acb586d8d2c715019b8b48111663e3b558a2a796
Michael Minhas <michael.minhas@hotmail.com>
2018-11-20 13:39:08 -0800

    login/signup

---
+++ controller/controller_signup.js
@@ -0,0 +1,11 @@
+addUser = () => {
+    console.log("sign up user");
+}
+
+loginUser = () => {
+    console.log("login user");
+}
+
+badgebook = () => {
+    console.log("connect with badgebook");
+}
